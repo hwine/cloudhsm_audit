596018a8b5e35486d3acba4513618bcc6704293c
Hal Wine <hwine@mozilla.com>
2020-11-11 13:31:36 -0800

    Initial commit - likely to be moved elsewhere

---
+++ parse_log.py
@@ -0,0 +1,169 @@
+# To add a new cell, type '# %%'
+# To add a new markdown cell, type '# %% [markdown]'
+# %%
+import os
+
+print(os.path.abspath(os.curdir))
+print(os.getcwd())
+# %%
+# read in the file
+with open("mgmt_log_1.working") as f:
+    log_lines = f.readlines()
+print(f"Processing {len(log_lines)} lines")
+
+
+# %%
+# utility routines
+log_line_index = 0
+
+
+def reset(to: int = 0) -> None:
+    global log_line_index
+    log_line_index = to
+
+
+def skip_to(text):
+    global log_line_index
+    while text not in log_lines[log_line_index]:
+        log_line_index += 1
+
+
+def get_line() -> str:
+    global log_line_index
+    line = log_lines[log_line_index]
+    log_line_index += 1
+    return line
+
+
+def skip_lines(count: int) -> None:
+    global log_line_index
+    log_line_index += count
+
+
+# %%
+# data structures
+from collections import namedtuple
+
+User = namedtuple("User", "user_id user_type user_name")
+
+
+# %%
+# get user info
+reset()
+skip_to("Number of users found")
+number_of_users = int(get_line().split(":")[1])
+print(f"found {number_of_users} users")
+skip_lines(2)
+users = {}
+for i in range(1, number_of_users + 1):
+    user_id, user_type, user_name, *_ = get_line().split()
+    assert int(user_id) == i
+    user = User(user_id, user_type, user_name)
+    users[i] = {"user": user}
+
+# remember where we finished
+start_of_info = log_line_index
+
+# %%
+# grab keys for user
+
+# regex for keys
+import re
+
+key_pattern = re.compile(
+    r"""
+    (?P<id>\d+)  # key id
+    (?:\(  # ignore the parens around the optional type
+        (?P<type>[os,]+)  # grab the type
+    \))?  # optional type string
+    ,? # separater
+""",
+    re.VERBOSE,
+)
+for i in users.keys():
+    reset(start_of_info)
+    skip_to(f"findAllKeys {i} 0")
+    skip_to("Number of keys found ")
+    number_of_keys = int(get_line().split()[-1])
+    if number_of_keys:
+        skip_lines(1)
+        key_line = get_line()
+        found_keys = []
+        for m in key_pattern.finditer(key_line):
+            found_keys.append(m.groupdict())
+        assert len(found_keys) == number_of_keys
+        users[i]["keys"] = found_keys
+
+
+# %%
+# now have all the data, report out anything we don't expect
+# Note that different results are reported if "releng" is considered
+# a key owner
+include_releng = False
+key_owners = [k for k, v in users.items() if v["user"].user_name.startswith("ko")]
+if include_releng:
+    key_owners.extend(
+        [k for k, v in users.items() if v["user"].user_name.startswith("releng")]
+    )
+
+# keep track of which keys are owned by wrong party
+from collections import defaultdict
+
+mis_owned_keys = defaultdict(list)
+for ko in key_owners:
+    user_name = users[ko]["user"].user_name
+    print(f"Checking {user_name} (id:{ko})")
+    # all keys owned, none shared to
+    for key in users[ko]["keys"]:
+        key_type = key["type"]
+        if key_type is None:
+            continue
+        elif "o" not in key_type:
+            print(f"""Error! {user_name} doesn't own {key["id"]}""")
+            mis_owned_keys[key["id"]].append({"id": ko, "name": user_name})
+
+print(f"{len(mis_owned_keys)} mis-owned keys")
+# %%
+
+key_by_id = {}
+
+
+def build_key_by_id() -> None:
+    global key_by_id
+    for user_id, v in users.items():
+        if "keys" not in v:
+            print(f"no 'keys' for {user_id}")
+            continue
+        for key in v["keys"]:
+            # ignore public keys (type == None)
+            key_type = key["type"]
+            if key_type and "o" in key_type:
+                if key["id"] in key_by_id:
+                    print(f"Error! multiple owners for {key['id']}")
+                key_by_id[key["id"]] = v["user"]
+            elif not key_type:
+                print(f"no type for {key}")
+
+
+build_key_by_id()
+
+
+def find_key_owner(id: int) -> User:
+    global key_by_id
+    try:
+        owner = key_by_id[id]
+    except KeyError:
+        print(f"No one owns key {id}")
+        owner = User(-1, "", "<unknown>")
+    return owner
+
+
+# %%
+# display current owners of mis-matched keys
+for k, v in mis_owned_keys.items():
+    owner = find_key_owner(k)
+    print(f"user {owner.user_id} ({owner.user_name}) owns key {k}")
+    print(f"""   should be owned by user {v[0]["id"]} ({v[0]["name"]})""")
+    assert len(v) == 1
+
+# %%
